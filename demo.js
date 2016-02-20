38ecd0f4fffc6bd96cf98c504d86295bf90f2d58
Antha Auciello <sauciello@panoramaed.com>
2016-02-20 17:59:48 -0500

    refactoring

--- demo.js
+++ demo.js
@@ -1,31 +1,3 @@
-const https = require('https');
+var app = require('./lib/app.js');
 
-var config = require(process.env.HOME + '/.boxconfig.json');
-
-var token = config.token;
-
-var options = {
-  hostname: 'api.dropboxapi.com',
-  path: '/2/files/list_folder',
-  method: 'POST',
-  headers: {
-    'User-Agent': 'box(github.com/olleicua/box)',
-    'Authorization': 'Bearer ' + token,
-    'Content-Type': 'application/json'
-  }
-};
-
-var req = https.request(options, function(res) {
-  console.log('statusCode: ', res.statusCode);
-  console.log('headers: ', res.headers);
-
-  res.on('data', function(d) {
-    process.stdout.write(d);
-  });
-});
-req.write(JSON.stringify({path:''}));
-req.end();
-
-req.on('error', function(e) {
-  console.error(e);
-});
+app.list('');
